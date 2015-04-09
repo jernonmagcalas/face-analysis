395e2bbe129c4b4b91a6a7e2f6419257073a47b0
aprilsacil <asacil@openovate.com>
2015-04-10 01:23:19 +0800

    some changes

--- server/index.php
+++ server/index.php
@@ -1,17 +1,31 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require_once __DIR__.'/faceAppSDK.php';
 
 $facepp = new Facepp();
 $facepp->api_key       = '83ec85fc12b1d2ca37067da2aa4b510a';
 $facepp->api_secret    = 'zQAUYqxBjotNNMaV7874l2mVMs4gZhPp';
 
+$_POST = json_decode(file_get_contents('php://input'),true);
 if(!isset($_POST['img'])) {
-    die('Error! Missing Img arguement');
+$error = array('error' => true, 'message' => 'Missing img arguement');
+    die(json_encode($error));
 }
 
+$root           = realpath(__DIR__.'/..');
+$path           = '/upload/';
+$file           = md5(time().floor(rand() * 1000000000000));
+$tmp            = explode(';', $_POST['img']);
+$extension      = str_replace('data:image/', '', array_shift($tmp));
+file_put_contents(
+        $root.$path.'/'.$file.'.'.$extension,
+        base64_decode(trim(str_replace('base64,', '', implode(';', $tmp)))));
+
+
 #detect local image 
-$params['img']          = $_POST['img'];
+$params['img']          = $root.$path.'/'.$file.'.'.$extension;
 $params['attribute']    = 'gender,age,race,smiling,glass,pose';
 
-$response               = $facepp->execute('/detection/detect',$params);
+$response               = $facepp->execute('/detection/detect', $params);
+unlink($root.$path.'/'.$file.'.'.$extension);
 die($response['body']);
